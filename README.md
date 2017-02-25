4d6db2391d979d25e334774f99382cf9303addec
andfroswe <gummianka88@gmail.com>
2017-02-25 18:39:32 +0100

    New and improved version with a GUI!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NewLinux
 Script for setting up a my environment for a clean Linux distro
-Current latest release: v1.0
+Current latest release: v2.0
 
 ## What it does
 This is a bash script that can be run on a clean linux distro to download and set up 
@@ -13,12 +13,9 @@ wget, though the downloading and unzipping needs to be done separately.
     $ curl -L https://github.com/andfroswe/newlinux/archive/[latest release].tar.gz | tar zx
     $ chmod u+x newlinux.sh
     $ sudo ./newlinux.sh
+    Use GUI options to get the necessary programs and installs.
 
 It should download all necessary files and programs via apt-get. It also adds 
 options to ~/.bashrc so be sure that the current bash config file is in that
 location.
 
-## What is not here
-
-- Downloading and building the latest texlive, it takes too long. Could perhaps
-be added as an option in the future.
--- newlinux.sh
+++ newlinux.sh
@@ -1,81 +1,186 @@
 #!/bin/bash
+# 
 
-# Script for installing essentials on a clean Unix computer
-# Also adds settings to .bashrc
-########################
-# Usage:
-# $chmod u+x newcomp.sh
-# $sudo ./newcomp.sh
-###############################
-# Author: Andreas Froderberg
-###########################################################
-# Current installs:
-# Vim with gui
-# Git
-# Python  2 and 3 with pip
-# Powerline fonts
-# Mupdf and xdotools
-
-# Options
-VIM="lua5.2 python python3 vim-gnome"
+##################################
+# Variables
+##################################
+# Install strings
+ALL_PROGRAMS=""
+VIM_DEPS="lua5.2"
+VIM_W_GUI="vim-gnome"
+VIM_NO_GUI="vim-nox"
 GIT="git"
 PYTHON="python python3 python-pip"
 MUPDF="mupdf xdotool"
 SSH="ssh"
 CTAGS="exuberant-ctags"
 UTILS="gddrescue xclip"
-# Combine all apps into one command
-ALL="$VIM $GIT $PYTHON $MUPDF $SSH $CTAGS $UTILS"
+TEXLIVE_ADDR="http://ftp.acc.umu.se/mirror/CTAN/systems/texlive/tlnet/" # Address to texlive download
+TEXLIVE_FILENAME="install-tl-unx.tar.gz" # Address to texlive download
 
-# Git options
-GITNAME="andfroswe"
-GITMAIL="gummianka88@gmail.com"
-MYREPO="https://github.com/andfroswe/" # Base of my git repo
 
-# Powerline fonts
-POWERLINE="https://github.com/powerline/fonts"
+# Install variables. 1 means that it will be installed
+# Change defaults here
+vim_install=1 # Install vim editor
+vim_gui=1 # vim_gnome or vim_tiny
+python_install=1 # Install python programming language
+git_install=1 # Install git
+mupdf_install=1 # Install mupdf pdf reader
+powerline_install=1 # Install powerline fonts
+utils_install=1 # Install some minor utility programs
+texlive_install=0 # Install and build texlive from source
+ros_install=0 # Install Robot Operating System
 
-##########################################################
-cd $HOME
-# Update and upgrade 
-echo "Starting clean download script..."
-echo "Updating repos..."
-sudo apt-get update > /dev/null
-echo "Upgrading..."
-sudo apt-get upgrade
+##################################
+# Functions
+##################################
 
-# Install programs
-echo "Installing apps... $ALL"
-sudo apt-get install -y $ALL
 
-# Setup Git config
-echo "Setting up Git..."
-git config --global user.name $GITNAME
-git config --global user.email $GITMAIL
-# To cache git password
-git config --global credential.helper cache
+# Reads the user input and sets variables
+read_choice() {
+    local choice
+    read -p "Enter your choice: " choice
+    case $choice in
+        1) let "vim_install=($vim_install+1)%2";;
+        2) let "vim_gui=($vim_gui+1)%2";;
+        3) let "python_install=($python_install+1)%2";;
+        4) let "git_install=($git_install+1)%2";;
+        5) let "mupdf_install=($mupdf_install+1)%2";;
+        6) let "utils_install=($utils_install+1)%2";;
+        7) let "powerline_install=($powerline_install+1)%2";;
+        8) let "texlive_install=($texlive_install+1)%2";;
+        9) let "ros_install=($ros_install+1)%2";;
+        0) run_all_installs ;;
+        999) exit ;;
+        *) echo "Invalid input"
+    esac
+}
 
-# Setup vim and vimrc
-echo "Setting up vim..."
-echo "Getting vimrcs from Git..."
-git clone "${MYREPO}vimcore"
-git clone "${MYREPO}vimplugs"
-echo "Creating redirect vimrc..."
-echo "source ~/vimcore/.vimrc" > ~/.vimrc
-echo "source ~/vimplugs/.vimrc" >> ~/.vimrc
+# Prints the menu
+show_menu() {
+    clear
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~ New linux install helper ~~~"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "Remember to run as SUDO!"
+    echo "1) Vim: "$vim_install
+    echo "2) Vim with GUI: "$vim_gui
+    echo "3) Python: "$python_install
+    echo "4) Git: "$git_install
+    echo "5) MuPDF and XDO: "$mupdf_install
+    echo "6) Utils: ddrescue, ctags, xclip: "$utils_install
+    echo "7) Powerline fonts: "$powerline_install
+    echo "8) Texlive: "$texlive_install
+    echo "9) ROS: "$ros_install
+    echo "0) Install and Exit"
+    echo "999) Exit without actions"
+}
 
-# Install powerline fonts
-echo "Installing powerline fonts..."
-git clone $POWERLINE
-cd fonts
-./install.sh
-cd $HOME
-rm fonts -rf
+# Logic for adding wanted programs
+run_all_installs() {
+    echo "******** Starting installs... **********"
+    echo "******** Upgdating repos...   **********"
+    apt-get update
+    echo "******** Upgrading ...        **********"
+    apt-get upgrade
+    echo "******** Installing from apt-get ...        **********"
+    install_programs
+    echo "******** Installs: $ALL_PROGRAMS *********"
+    apt-get install $ALL_PROGRAMS
+    # If git was installed, configure it
+    if [[ $git_install == 1 ]]; then
+    echo "******** Configuring git....        **********"
+        git_config
+    fi
+    # Install powerline fonts. Requires git
+    if [[ $git_install == 1 && $powerline_install == 1 ]]; then
+        echo "******** Installing powerline fonts from git... **********"
+        install_powerline_fonts
+    fi
+    # Install texlive if chosen
+    if [[ $texlive_install == 1 ]]; then 
+        echo "******** Installing texlive... **********"
+        install_texlive 
+    fi
+    # Install ros if chosen
+    if [[ $ros_install == 1 ]]; then
+        echo "******** Installing ROS... **********"
+        install_ros
+    fi
+    echo "****************** DONE! ****************"
+    source ~/.bashrc
 
-# Add settings to .bashrc
-echo "Adding settings to .bashrc..."
-echo "# Git" >> ~/.bashrc
-echo "function Gclone { git clone https://github.com/andfroswe/\$1; }" >> ~/.bashrc
+    exit
+}
 
-# Done!
-echo "Done, terminating..."
+install_programs() {
+    ## Add all wanted programs to install string
+    # Vim variables
+    if [[ $vim_install == 1 ]]; then 
+        ALL_PROGRAMS+="$VIM_DEPS $PYTHON "
+        # Set gui or no gui
+        if [[ $vim_gui == 1 ]]; then
+            ALL_PROGRAMS+="$VIM_W_GUI "
+        else
+            ALL_PROGRAMS+="$VIM_NO_GUI "
+        fi
+    fi
+    # Add programs to installation
+    if [[ $python_install == 1 ]]; then ALL_PROGRAMS+="$PYTHON "; fi #Python programming language
+    if [[ $git_install == 1 ]]; then ALL_PROGRAMS+="$GIT $SSH "; fi #Git code revisioning
+    if [[ $mupdf_install == 1 ]]; then ALL_PROGRAMS+="$MUPDF "; fi #Mupdf light pdf reader
+    if [[ $utils_install == 1 ]]; then ALL_PROGRAMS+="$UTILS $CTAGS "; fi #Install minor utilities
+}
+
+# Git options and add function to clone from my repo
+git_config() {
+    echo "Git config"
+    local GITNAME="andfroswe"
+    local GITMAIL="gummianka88@gmail.com"
+    local MYREPO="https://github.com/andfroswe/" # Base of my git repo
+    echo "Setting up Git..."
+    git config --global user.name $GITNAME
+    git config --global user.email $GITMAIL
+    # To cache git password
+    git config --global credential.helper cache
+    # Add settings to .bashrc for shortcut to clone from my repo
+    echo "Adding settings to .bashrc..."
+    echo "" >> ~/.bashrc
+    echo "# Git settings" >> ~/.bashrc
+    echo "function Gclone { git clone $MYREPO\$1; }" >> ~/.bashrc
+}
+ #Install powerline fonts
+install_powerline_fonts() {
+    echo "Powerline config"
+    git clone $POWERLINE
+    cd fonts
+    ./install.sh
+    cd $HOME
+    rm fonts -rf
+}
+# Download and build texlive
+install_texlive() {
+    cd ~/Downloads
+    wget $TEXLIVE_ADDR$TEXLIVE_FILENAME
+    tar xvzf $TEXLIVE_FILENAME
+    echo "******* Manually enter folder and run install.sh *********"
+}
+# Download and install ROS
+install_ros() {
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    apt-get update
+    apt-get install ros-kinetic-desktop-full
+    rosdep init
+    rosdep update
+    echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+    apt-get install python-rosinstall
+}
+
+### User interface loop ###
+while true
+do
+    show_menu
+    read_choice
+done
+
