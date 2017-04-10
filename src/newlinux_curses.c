106684e1770f86adf40d4575fb82f7919edc2697
andfroswe <gummianka88@gmail.com>
2017-04-10 09:48:08 +0200

    Made working curses UI. Still missing some features but this works

--- src/newlinux_curses.c
+++ src/newlinux_curses.c
@@ -20,11 +20,15 @@
 #include <menu.h>
 #include <stdlib.h>
 
+#define MENU_W 40 // Menu width
+#define LENGTH_OF_ARRAY(a) (sizeof(a)/sizeof(a[0]))
+
 // Forward declarations
 void installVim(bool gui);
 void installGit();
 void configGit();
 void installProgrammingTools();
+void installUtils();
 
 // System command strings
 const char install[] = "sudo apt install -y ";
@@ -34,6 +38,7 @@ const char s_vim_gui[] = "vim-gnome";
 const char s_git[] = "git";
 const char s_python[] = "python python3 python-pip";
 const char s_ptools[] = "exuberant-ctags ";
+const char s_utils[] = "gddrescue xclip tmux";
 
 // These two have to be synced with each other for indexing to work
 // Last enum is the number of enums
@@ -42,14 +47,16 @@ typedef enum Programs {
     vim_gui,
     git,
     program_tools,
+    utils,
     NO_PROGRAMS
 } programs;
 
 char *choices[] = {
     "vim",
     "vim_gui",
+    "git",
     "programming_tools",
-    "git"
+    "utils"
 };
 
 int main(int argc, char *argv[])
@@ -60,17 +67,19 @@ int main(int argc, char *argv[])
     noecho(); // Don't echo entered keys
     keypad(stdscr, TRUE); // Enable function keys
 
-    // Menu parameters
+    // Curses entities
     ITEM *cur_item; // Pointer for current chosen item
     MENU *my_menu; // Menu
+    WINDOW *menu_window; // Window to contain menu
 
     // Create array of pointers to items
     ITEM **my_items = (ITEM**) calloc (NO_PROGRAMS, sizeof(ITEM *));
     // Fill up with new items
     for (int i = 0; i < NO_PROGRAMS; ++i) 
     { 
-        //            new_item(name      , description)
-        my_items[i] = new_item(choices[i], (char *) NULL);
+        my_items[i] = new_item(choices[i], // Name
+                              (char *) NULL // Description
+                              );
     }
     my_items[NO_PROGRAMS] = (ITEM*)NULL;
     my_menu = new_menu((ITEM **) my_items);
@@ -78,12 +87,35 @@ int main(int argc, char *argv[])
     // Make menu multivalued
     menu_opts_off(my_menu, O_ONEVALUE);
 
+    // Create window for menu
+    // The menu needs to be put in the subwindow for it to be displayed properly
+    menu_window = newwin(NO_PROGRAMS + 2, // Height
+                        MENU_W, // Width
+                        LINES/2 - (NO_PROGRAMS + 2)/2, // Starty
+                        COLS/2 - MENU_W/2 // Startx
+                        );
+    keypad(menu_window, TRUE); // Enable function keys
+    set_menu_win(my_menu, menu_window); // Attach menu to window
+    set_menu_sub(my_menu, derwin(menu_window, 
+                                NO_PROGRAMS, // Height
+                                MENU_W -2, // Width
+                                1, // Starty relative to parent
+                                1 // Startx relative to parent
+                                )); // Set the menu inside the menu window
+    set_menu_mark(my_menu, "*"); // Set mark for chosen itmes
+    box(menu_window, 0, 0); // Draw a box around the window
+
     // Print frame and standard items
-	mvprintw(LINES - 3, 0, "Use <SPACE> to select or unselect an item.");
-	mvprintw(LINES - 2, 0, "<ENTER> to install selected items.");
-	mvprintw(LINES - 1, 0, "q to exit without saving.");
+    char title[] = "New Linux install helper: Because I\'m too lazy to type";
+    mvprintw(1, COLS/2 - LENGTH_OF_ARRAY(title)/2, title);
+	mvprintw(LINES - 4, 0, "Use <TAB> to select or unselect an item.");
+	mvprintw(LINES - 3, 0, "<ENTER> to install selected items.");
+	mvprintw(LINES - 2, 0, "q to exit without saving.");
+    box(stdscr, 0, 0); // Draw a border around main window
+    refresh(); // Draw main window
+    // Draw menu window
 	post_menu(my_menu);
-	refresh();
+	wrefresh(menu_window);
 
     // Main loop
     int c; // Character read from keyboard
@@ -94,10 +126,16 @@ int main(int argc, char *argv[])
             case KEY_DOWN:
                 menu_driver(my_menu, REQ_DOWN_ITEM);
                 break;
+            case 'j':
+                menu_driver(my_menu, REQ_DOWN_ITEM);
+                break;
             case KEY_UP:
                 menu_driver(my_menu, REQ_UP_ITEM);
                 break;
-            case ' ':
+            case 'k':
+                menu_driver(my_menu, REQ_UP_ITEM);
+                break;
+            case 9: // Tab
                 menu_driver(my_menu, REQ_TOGGLE_ITEM);
                 break;
             case 10: // Enter key
@@ -125,6 +163,11 @@ int main(int argc, char *argv[])
                     {
                         installProgrammingTools();
                     }
+                    // Install utilities
+                    if (item_value(items[utils]) == TRUE)
+                    {
+                        installUtils();
+                    }
 
                     printf("******** Done! *********\n");
                     printf("********* Press q to quit ***********\n");
@@ -132,8 +175,8 @@ int main(int argc, char *argv[])
                     refresh();
                     break;
                 }
-
-        }
+            }
+        wrefresh(menu_window);
     }
 
     
@@ -194,4 +237,12 @@ void installProgrammingTools()
     s = "";
     printf("****** Installing programming tools ********\n");
     system(strcat(s, s_ptools));
+}
+
+void installUtils()
+{
+    char *s = ""; // Temporary string
+    strcpy(s, install); // Copy install string into s
+    printf("****** Installing utilities *******\n");
+    system(strcat(s, s_utils));
 }
